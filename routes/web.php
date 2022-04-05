05b2dc5fb45eb45bb20698313c6444bdd846f283
Edgardo Alvarez <edgardo@livedesign.org>
2022-04-04 21:52:51 -0500

    “update”

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get("/adminlte", function(){
+    return view("admin.dashboard");
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
